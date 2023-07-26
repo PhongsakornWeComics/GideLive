1545ed16081d84bc5e4f144a7ab61d72bf5f97ed
Phongsakorn Srikate <>
2023-07-26 22:18:56 +0700

    first commit

---
+++ MuMu_Project/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MuMu_Project
+//
+//  Created by Phongsakorn Srikate on 3/6/2566 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MuMu_Project/MuMu_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MuMu_ProjectApp.swift
+//  MuMu_Project
+//
+//  Created by Phongsakorn Srikate on 3/6/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct MuMu_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
